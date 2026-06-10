54543b140dbcb9b1ad90729d39556b70487b118d
alex <berezov86@gmail.com>
2026-06-10 13:17:44 +0300

    rename file + add source

---
+++ bash/scripts/habr.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# code takes from https://habr.com/ru/companies/ruvds/articles/325522/
+
+# our comment is here
+# echo "The current directory is:"
+# pwd
+# echo "The user logged in is:"
+# whoami
+
