b451152b67b88f43eb9cdb77c4218d69c09241aa
alex <berezov86@gmail.com>
2026-06-11 13:17:14 +0300

    refactor part2_habr.sh: uncomment and enhance logic to list executable files in PATH

--- bash/scripts/part2_habr.sh
+++ bash/scripts/part2_habr.sh
@@ -70,3 +70,27 @@
 
 
 # files
+# IFS=$'\n'
+# for entry in $(cat /etc/passwd)
+# do
+# 	echo "Values in $entry –"
+# 	IFS=:
+# 	for value in $entry
+# 	do
+# 		echo " $value"
+# 	done
+# done
+
+
+IFS=:
+for folder in $PATH
+do
+	echo "$folder:"
+	for file in $folder/*
+	do
+		if [ -x $file ]
+		then
+			echo " $file"
+		fi
+	done
+done
