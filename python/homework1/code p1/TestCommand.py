46b8f79591ed6c614453d80cda275cd2f4a3bedf
Alex B <berezov86@gmail.com>
2026-06-18 12:28:44 +0300

    feat: add initial implementation of TestCommand.py for Python command usage examples

---
+++ python/homework1/code p1/TestCommand.py
@@ -0,0 +1,14 @@
+# задание из Python_Руководство_my_р1_среда_разработки.pdf
+# code "p1"
+
+# Применение команд Python
+
+import math
+
+# help(math)
+# print(dir(math))
+# help(math.sqrt)
+
+list1 = [82, 8, 23, 97, 92]
+# print(dir(list1))q
+help(list1.insert)
