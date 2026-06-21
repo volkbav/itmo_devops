02275d10011bd7d9fa44f743052a693d35846eed
volkbav <berezov86@gmail.com>
2026-06-21 21:43:20 +0300

    fix: update project name in Project1_1B.py for consistency

--- python/module1/code 1B/Project1_1B.py
+++ python/module1/code 1B/Project1_1B.py
@@ -1,7 +1,7 @@
 # задание из Задания_Number_B
 # code "1B"
 
-# Project1_1
+# Project1_1B
 name, surname = map(str, input('введите ваше Имя и Фамилию: ').split())
 name, surname = name.title(), surname.title()
 login = surname + name
