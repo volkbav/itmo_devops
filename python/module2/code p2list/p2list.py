e561c5a83df10d3e0bcd1b9ea7e861bfc05aee7a
volkbav <berezov86@gmail.com>
2026-06-21 20:28:15 +0300

    feat: add p2list.py with random list generation example

---
+++ python/module2/code p2list/p2list.py
@@ -0,0 +1,18 @@
+# задание из Задания_Number_B
+# code "p2list"
+
+# Списки
+
+import random
+
+list1 = [random.randint(x, 100) for x in range(10)]
+print(list1)
+
+# print(dir(list))
+
+# help(list.insert)
+# help(list.append)
+# help(list.sort)
+# help(list.remove)
+# help(list.reverse)
+
