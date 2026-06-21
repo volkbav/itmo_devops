342b46aa44f5900e993fa876969a948e24fc279d
volkbav <berezov86@gmail.com>
2026-06-21 21:47:09 +0300

    feat: implement product, cost, and employee lists with zip function for display

--- python/module3/code 2B/Project1_2B.py
+++ python/module3/code 2B/Project1_2B.py
@@ -2,3 +2,10 @@
 # code "1B"
 
 # Project1_2B
+
+product = ['apple', 'milk', 'tea']
+cost = [150, 90, 120]
+employee = ['vasia', 'fedia', 'ivan ivanish']
+
+for i in zip(product, cost, employee):
+    print(i)
