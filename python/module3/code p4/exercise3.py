459f53f08bf19b258e67e6515ccd0bbae06fbba4
alex <berezov86@gmail.com>
2026-06-22 13:46:32 +0300

    update exercise3 in module3

--- python/module3/code p4/exercise3.py
+++ python/module3/code p4/exercise3.py
@@ -8,10 +8,38 @@ from random import randint  # подключение функции из мод�
 total = 100  # запас ресурса
 i = 0  # счетчик итераций цикла
 while i < 5:
-    n = randint(1, 50)  # имитация расхода ресурса
+    n = randint(1, 20)  # имитация расхода ресурса
     total = total - n
-    i = i + 1
+    i += 1
 
 print("Осталось", total)
 
 # Контроль ресурса – прерывание цикла
+total = 100
+i = 0
+while i < 5:
+    n = randint(1, 50)
+    total = total - n
+    if total < 0:
+        total = 0
+        break
+    i += 1
+
+print("Осталось", total)
+
+
+# Контроль процесса – выполнился ли цикл полностью?
+total = 100
+i = 0
+while i < 5:
+    n = randint(1, 50)  # имитация расхода ресурса
+    total = total - n
+    if total < 0:
+        total = 0
+        print("Прерывание процесса")
+        break
+    i = i + 1
+else:
+    print("Процесс выполнен полностью")
+
+print("Осталось", total)
