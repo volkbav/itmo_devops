20a7840e0f459a48030b36a6366cc402ef738d8d
alex <berezov86@gmail.com>
2026-06-22 17:02:35 +0300

    add Project1_3B  in module3/code 3B

---
+++ python/module3/code 3B/Project1_3B.py
@@ -0,0 +1,11 @@
+# задание из Задания_Number_B
+# code "3B"
+
+# Project1_3B
+
+product = ['apple', 'milk', 'tea']
+cost = [150, 90, 120]
+employee = ['vasia', 'fedia', 'ivan ivanish']
+
+for i in zip(product, cost, employee):
+    print(i)
