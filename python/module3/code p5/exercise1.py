dd8dca7a22ad30e543c1689e2851f639adf83757
alex <berezov86@gmail.com>
2026-06-22 16:29:59 +0300

    update exercise1 in module3/code p5

--- python/module3/code p5/exercise1.py
+++ python/module3/code p5/exercise1.py
@@ -1,5 +1,25 @@
 # Python_Руководство_my_р5_списки_циклы
-# code "p4"
+# code "p5"
 
 # Упражнение 1. Использование списков
 
+fruits = ['Apple', 'Grape', 'Peach', 'Banan', 'Orange']
+
+def print_list(list_):
+    for i in list_:
+        print(i)
+
+
+print_list(fruits)
+
+fruits[1] = fruits[0] = 'Watermelon'
+fruits[3] = 'Lemon'
+
+fruits.append('Banan')
+
+if "Apple" in fruits:
+    print("В списке есть 'Apple'")
+else:
+    print("В списке нет 'Apple'")
+
+print(fruits)
