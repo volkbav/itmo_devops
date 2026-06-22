9c5de39679a9b323c929748b3e5357310a2bda5a
alex <berezov86@gmail.com>
2026-06-22 16:38:14 +0300

    add control_work in module3/code p5

---
+++ python/module3/code p5/control_work.py
@@ -0,0 +1,14 @@
+# Python_Руководство_my_р5_списки_циклы
+# code "p5"
+
+# Упражнение 4.1 (контрольное) Объем продаж
+
+weekday = [
+    "Monday", 
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday", 
+    "Saturday", 
+    "Sunday"]
+
