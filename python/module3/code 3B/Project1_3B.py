02ea102c6acb5009a8cc48e048132e71a603eb0b
alex <berezov86@gmail.com>
2026-06-22 17:23:13 +0300

    update Project1_3B  in module3/code 3B

--- python/module3/code 3B/Project1_3B.py
+++ python/module3/code 3B/Project1_3B.py
@@ -3,3 +3,20 @@
 
 # Project1_3B
 
+import random
+
+# Задание 1
+
+cutoff = random.randint(1, 100)
+
+
+list_len = random.randint(3, 10)
+num_list = []
+for i in range(list_len):
+    num_list.append(random.randint(1, 100))
+
+print(num_list)
+
+new_list = ['High' if i < cutoff else 'Low' for i in num_list]
+print(f'cutof = {cutoff}\n', new_list)
+
