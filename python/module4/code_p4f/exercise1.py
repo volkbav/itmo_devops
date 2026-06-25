38c8ba30f3992449c0c9e9bebe5c2f49858d65cc
alex <berezov86@gmail.com>
2026-06-25 16:25:41 +0300

    update exercise1  in module4/code p4f

--- python/module4/code_p4f/exercise1.py
+++ python/module4/code_p4f/exercise1.py
@@ -2,3 +2,14 @@
 # code "p4f"
 
 # Упражнение 1. Поиск слова в текстовом файле
+import os
+from pathlib import Path
+
+# base_dir = Path(__file__).resolve().parent
+# print(str(base_dir))
+
+# folder = base_dir + '\PythonPrim\Textfiles'
+# print(folder)
+
+folder = os.getcwd() + '\PythonPrim\Textfiles'
+print(path)
