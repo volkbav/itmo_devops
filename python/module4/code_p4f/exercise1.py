6255c7a556d76e21e6fccbf618826fc5dbc7fd30
alex <berezov86@gmail.com>
2026-06-25 16:27:14 +0300

    update exercise1  in module4/code p4f

--- python/module4/code_p4f/exercise1.py
+++ python/module4/code_p4f/exercise1.py
@@ -8,8 +8,8 @@ from pathlib import Path
 # base_dir = Path(__file__).resolve().parent
 # print(str(base_dir))
 
-# folder = base_dir + '\PythonPrim\Textfiles'
+# folder = base_dir + '\\PythonPrim\\Textfiles'
 # print(folder)
 
-folder = os.getcwd() + '\PythonPrim\Textfiles'
-print(path)
+folder = os.getcwd() + '\\PythonPrim\\Textfiles'
+print(folder)
