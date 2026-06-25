28ebaecafb664a29cd4c8d0a6805f62983f6792e
alex <berezov86@gmail.com>
2026-06-25 11:30:05 +0300

    update  Project1_3B  in module3/code 3B

--- python/module3/code 3B/Project1_3B.py
+++ python/module3/code 3B/Project1_3B.py
@@ -20,3 +20,31 @@ print(num_list)
 new_list = ['High' if i < cutoff else 'Low' for i in num_list]
 print(f'cutoff = {cutoff}\n', new_list)
 
+
+# Задание 2
+import names  # type: ignore # noqa: E402
+
+name_list = [names.get_first_name() for i in range(100)]
+
+A_M_names = []
+other_names = []
+
+[(A_M_names if i[0] in "AM" else other_names).append(i) for i in name_list]
+
+print(f'{name_list},\nA and M names\n{A_M_names}, \nother names\n{other_names}')
+
+
+# Задание 3
+
+cli = "введите 1 слово "
+words = []
+word = ""
+inbox = "not empty"
+while inbox != '':
+    inbox = input(cli)
+    if inbox == "":
+        break
+    words.append(inbox)
+    word += inbox[0]
+    cli = 'введите следующее слово '
+print(f'{words}, {word}')
