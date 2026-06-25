b1863bb80a1ef7c370ffc810154a4eb185499076
alex <berezov86@gmail.com>
2026-06-25 16:42:05 +0300

    update exercise1  in module4/code p4f

--- python/module4/code_p4f/exercise1.py
+++ python/module4/code_p4f/exercise1.py
@@ -5,23 +5,23 @@
 import os
 from pathlib import Path
 
-# base_dir = Path(__file__).resolve().parent
-# print(str(base_dir))
-
-# folder = base_dir + '\\PythonPrim\\Textfiles'
-# print(folder)
-
 # выбор пути для win/linux
-if os.name == 'posix':
-    folder = os.getcwd() + '/PythonPrim/Textfiles'
-else:
-    folder = os.getcwd() + '\\PythonPrim\\Textfiles'
-print(folder)
+sep = os.path.sep  # разделители для win (\) и для unix (/)
+folder = os.getcwd() + f'{sep}PythonPrim{sep}Textfiles'
+
 
 answ = set()
 search = input('Поиск: ')
 
 for filename in os.listdir(folder):
     filepath = os.path.join(folder, filename)
-    print(filepath)
+    with open(filepath, 'r', encoding='utf-8') as f:
+        for line in f:
+            if search in line:
+                answ.add(filename)
+
+for i in answ:
+    print(i)
+
+
 
