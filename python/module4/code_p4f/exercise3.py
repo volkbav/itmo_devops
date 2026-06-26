30ba592b13680c8cf6498d4c7f65c12b094f4091
alex <berezov86@gmail.com>
2026-06-26 15:02:16 +0300

    update exercise3  in module4/code p4f

--- python/module4/code_p4f/exercise3.py
+++ python/module4/code_p4f/exercise3.py
@@ -15,8 +15,24 @@ base_dir = Path(__file__).parent.resolve()
 home = Path.home()  # получение ссылки для домашнего каталога
 print(home)  # проверка, что получили нужный каталог
 
-my_folder = base_dir / 'my_folder'
-if not my_folder.exists():
-    my_folder.mkdir()
 
+def add_folder(base_dir, folder_name):
+    folder_name = base_dir / folder_name
+    if not folder_name.exists():
+        folder_name.mkdir()
+    return folder_name
+
+
+my_folder = add_folder(base_dir, 'my_folder')
+
+file1 = my_folder / 'file1.txt'
+file1.touch()
+(my_folder / 'file2.txt').touch()
+my_folder.joinpath('image.png').touch()
+
+images = add_folder(my_folder, 'images')
+
+for f in my_folder.glob('*.png'):
+    path_destination = images / f.name
+    f.replace(path_destination)
 
