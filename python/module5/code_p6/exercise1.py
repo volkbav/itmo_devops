fa89280df8afe1f96f12a363e1c787e04e7f7326
volkbav <berezov86@gmail.com>
2026-06-27 20:44:46 +0300

    Add additional examples for using built-in mathematical functions

--- python/module5/code_p6/exercise1.py
+++ python/module5/code_p6/exercise1.py
@@ -7,3 +7,16 @@
 a, b, c = map(int, input('введите 3 числа через пробел ').split())
 
 print(max(a, b, c))
+
+
+# Задание 2. Использование других функций
+import math
+
+help(math)
+a = float(input("Введите число: "))
+y1 = math.sqrt(a)
+y = math.ceil(y1)
+print('sqrt(a) = ', y1)
+print('sqrt(a) = ', y)
+y = math.ceil(math.sqrt(a))
+print('sqrt(a) = ', y)
