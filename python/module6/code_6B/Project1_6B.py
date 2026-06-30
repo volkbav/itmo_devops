51a2e65f137605b826d49c9000b2f9176af42429
alex <berezov86@gmail.com>
2026-06-30 09:40:23 +0300

    update Priject1_6B

--- python/module6/code_6B/Project1_6B.py
+++ python/module6/code_6B/Project1_6B.py
@@ -3,3 +3,20 @@
 
 # Project 1_6B
 
+from datetime import date
+from pathlib import Path
+
+import app_logger
+
+FILES_LIST = ['data_01_.csv', 'data_01.csv', 'data_02.csv']
+
+logger = app_logger.get_logger(__name__)
+
+
+# path to the files 
+base_dir = Path(__file__).parent.resolve()
+
+print(base_dir)
+logger.info('test info')
+
+logger.warning("test warning")
