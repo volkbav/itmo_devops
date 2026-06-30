eb3884a06a435ef30eedae051beec43eb21d50f1
alex <berezov86@gmail.com>
2026-06-30 11:18:56 +0300

    update Project1_6B

--- python/module6/code_6B/Project1_6B.py
+++ python/module6/code_6B/Project1_6B.py
@@ -3,57 +3,59 @@
 
 # Project 1_6B
 
+import csv
 from datetime import datetime
-from pathlib import Path
 
-import app_logger, csv
+import app_logger
 
-FILES_LIST = ['data_01_.csv', 'data_01.csv', 'data_02.csv']
+FILES_LIST = ['data_01_.csv', 'data_01.csv', 'not_exist.csv', 'data_02.csv']
 
 logger = app_logger.get_logger(__name__)
 
 
-# path to the files 
-base_dir = Path(__file__).parent.resolve()
-
-
 def read_csv(file: str) -> list:
     # try to open file
+    data = []
     try:
         with open(file) as f:
             reader = csv.reader(f)
             logger.info(f'open file {f}')
             headers = next(reader)
-            data = []
             data.append(headers)
             for row in reader:
                 data.append(row)
             return data
     except FileNotFoundError:
-        logger.warning("File not found")
+        logger.warning(f"File not found: {file}")
+    return []
 
 
-def write_csv(data: list):
-    file_name = str(datetime.now()) + ' backup.csv'
-    with open(file_name, 'w+') as f:
-        writer = csv.writer(f)
+def write_csv(data: list, name='_backup'):
+    if not data:
+        return
+     
+    file_name = datetime.now().strftime("%Y%m%d_%H%M%S") + f'_{name}.txt'
+    with open(
+        file_name, 
+        'w',
+        encoding="utf-8"
+    ) as f:
+        # writer = csv.writer(f)
         for row in data:
-            writer.writerow(fow)
+            f.write(','.join(row) + '\n')
+        logger.info(f"Copied {name} successfully")
+   
 
-
-print(read_csv('data_01_.csv'))
-print(write_csv(read_csv('data_01_.csv')))
-# def copy_data(file: str):
-
-    
+def copy_files(files: list):
+    for file in files:
+        msg = str(datetime.now()) + f' start copy file {file}'
+        logger.info(msg)
+        write_csv(read_csv(file), file)
         
-        
 
-
+copy_files(FILES_LIST)
 
 # test print
-# print(base_dir)
-# logger.info('test info')
-
-# logger.warning("test warning")
+# print(read_csv('data_01_.csv'))
+# print(write_csv(read_csv('data_01_.csv')))
 # ---
