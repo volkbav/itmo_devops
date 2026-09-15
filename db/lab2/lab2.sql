753157e938e25b0521d3d31c27536a869b7caced
Alex <berezov86@gmail.com>
2026-09-15 18:48:35 +0300

    feat: update SQL script for Задание 3 with queries for aircrafts and bookings

--- db/lab2/lab2.sql
+++ db/lab2/lab2.sql
@@ -1,1 +1,21 @@
--- Задание 2
+-- Задание 3
+-- 1. отображение всех столбцов в таблице aircrafts
+SELECT * FROM aircrafts;
+
+-- 2. отображение только выбранные столбцы в таблице aircrafts
+SELECT aircraft_code,
+    model
+FROM aircrafts;
+
+-- 3. получение конкретных строк в таблице
+SELECT model, range
+FROM bookings,aircrafts_data
+WHERE range < 5000;
+
+-- 4. фильтрация данных с помощью сравнения строк
+SELECT book_ref, passenger_id, passenger_name
+FROM bookings.tickets
+WHERE passenger_name LIKE 'V%'
+    OR passenger_name LIKE 'E%'
+
+--
