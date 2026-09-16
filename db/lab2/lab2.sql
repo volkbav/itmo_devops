64e471c0a005dcecf888ed0c51f5469cbb9bfc15
Alex <berezov86@gmail.com>
2026-09-16 16:51:58 +0300

    feat: add SQL queries for handling NULL values in flight data

--- db/lab2/lab2.sql
+++ db/lab2/lab2.sql
@@ -16,7 +16,7 @@ WHERE range < 5000;
 SELECT book_ref, passenger_id, passenger_name
 FROM bookings.tickets
 WHERE passenger_name LIKE 'V%'
-    OR passenger_name LIKE 'E%'
+    OR passenger_name LIKE 'E%';
 
 -- 5. Получение диапазона значения
 SELECT flight_no, scheduled_departure, scheduled_arrival,
@@ -32,3 +32,47 @@ FROM bookings.flights
 WHERE departure_airport = 'DME'
     AND arrival_airport IN ('LED', 'KZN')
     AND scheduled_departure between '2017-08-31' and '2017-09-01';
+
+-- 7. Работа со значениями NULL
+SELECT
+    flight_no,
+    scheduled_departure,
+    scheduled_arrival,
+    actual_departure,
+    actual_arrival
+FROM bookings.flights
+WHERE departure_airport = 'DME'
+    AND actual_departure = NULL;
+
+
+SELECT
+    flight_no,
+    scheduled_departure,
+    scheduled_arrival,
+    actual_departure,
+    actual_arrival
+FROM bookings.flights
+WHERE departure_airport = 'DME'
+    AND actual_departure IS NULL;
+
+
+SELECT
+    flight_no,
+    scheduled_departure,
+    scheduled_arrival,
+    COALESCE(actual_departure, '9999-12-31'),
+    COALESCE(actual_arrival, '9999-12-31')
+FROM bookings.flights
+WHERE departure_airport = 'DME'
+    AND arrival_airport = 'KZN';
+
+
+SELECT
+    flight_no,
+    scheduled_departure,
+    scheduled_arrival,
+    COALESCE(actual_departure, '9999-12-31') AS "Actual Departure",
+    COALESCE(actual_arrival, '9999-12-31') "Actual Arrival"
+FROM bookings.flights
+WHERE departure_airport = 'DME'
+    AND arrival_airport = 'KZN';
