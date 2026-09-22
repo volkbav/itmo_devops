e832ccb673d738341c83ce4fe1ce4ce418496d48
Alex <berezov86@gmail.com>
2026-09-22 15:26:42 +0300

    feat: add SQL insert statement for shares in lab5-1.sql

---
+++ db/lab5/lab5-1.sql
@@ -0,0 +1,14 @@
+-- задание 1
+INSERT INTO "ShareDetails".shares(
+    sharedesc
+    ,sharetickerid
+    ,currentprice
+)
+VALUES (
+    'ACME''S HOMEBAKE COOKISE INK'
+    ,'AHCI'
+    ,2.34125
+);
+
+SELECT * FROM "ShareDetails".shares;
+
