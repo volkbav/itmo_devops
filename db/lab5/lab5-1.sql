8b18234b5f2791f44859980ccac22063f1a51ac8
Alex <berezov86@gmail.com>
2026-09-22 15:59:35 +0300

    feat: update lab5-1.sql to modify customerid handling and add new customer entries

--- db/lab5/lab5-1.sql
+++ db/lab5/lab5-1.sql
@@ -90,3 +90,35 @@ CREATE UNIQUE INDEX IF NOT EXISTS ix_customers_customerid
 
 SELECT * FROM "CustomerDetails".customers
 
+ALTER TABLE "CustomerDetails".customers
+ALTER COLUMN customerid
+DROP IDENTITY;
+
+UPDATE "CustomerDetails".customers
+SET customerid = 1;
+
+
+ALTER TABLE "CustomerDetails".customers
+ALTER COLUMN customerid
+ADD GENERATED ALWAYS AS IDENTITY;
+
+
+INSERT INTO "CustomerDetails".customers (
+    customertitleid
+    ,customerlastname
+    ,customerfirstname
+    ,customerotherinitials
+    ,addressid
+    ,accauntnumber
+    ,accaunttypeid
+    ,clearedbalance
+    ,unclearedbalance
+)
+VALUES 
+    (1, 'Brust', 'Andrew', 'J.', 133, 18176111, 1, 200.00, 2.00),
+    (3, 'Lobel', 'Leonard', NULL, 145, 53431993, 1, 437.97, -10.56)
+;
+
+ALTER SEQUENCE "CustomerDetails".customers_customerid_seq RESTART WITH 2;
+
+-- задание 2
